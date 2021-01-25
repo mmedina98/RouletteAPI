586f99f1fc58beb83dc0473edfdef683c7ab214d
Michael Medina <msmed@DESKTOP-Q7JNDK6>
2021-01-24 20:44:34 -0500

    Plantilla proyecto

---
+++ RouletteAPI/Controllers/RouletteController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RouletteAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RouletteController : ControllerBase
+    {
+
+        public RouletteController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return null;
+        }
+    }
+}
